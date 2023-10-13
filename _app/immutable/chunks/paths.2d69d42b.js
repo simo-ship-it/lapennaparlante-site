897005af02611e97d9d95dd68d08fa3ed991de6a
simonebilleri <simonebilleri2@gmail.com>
2023-10-13 22:42:52 +0200

    prova1

---
+++ _app/immutable/chunks/paths.2d69d42b.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1oxuqyb)==null?void 0:s.base)??"/lapennaparlante";var a;const t=((a=globalThis.__sveltekit_1oxuqyb)==null?void 0:a.assets)??e;export{t as a,e as b};
